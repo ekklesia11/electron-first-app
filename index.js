4e5a9cfb1e1307010951f227b1e915db489621a9
Daniel Park <ekklesia89@gmail.com>
2020-07-01 22:51:17 +0900

    first shopping list app updated

--- index.js
+++ index.js
@@ -1,43 +1,126 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, Menu, ipcMain } = require("electron");
+const url = require("url");
+const path = require("path");
 
-function createWindow() {
-  // Create the browser window.
-  let win = new BrowserWindow({
-    width: 800,
-    height: 600,
+// Set env
+process.env.NODE_ENV = "production";
+
+let mainWindow;
+let addWindow;
+
+// listen for app to be ready
+
+app.on("ready", function () {
+  // Create new window
+  mainWindow = new BrowserWindow({
     webPreferences: {
       nodeIntegration: true,
     },
   });
 
-  // and load the index.html of the app.
-  win.loadFile("index.html");
+  // Load html into window
+  mainWindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname, "index.html"),
+      protocol: "file:",
+      slashes: true,
+    })
+  ); // file://dirname/index.html
 
-  // Open the DevTools.
-  win.webContents.openDevTools();
+  // Quit app when closed
+  mainWindow.on("closed", function () {
+    app.quit();
+  });
+
+  // Build menu from template
+  const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+
+  // Insert menu
+  Menu.setApplicationMenu(mainMenu);
+});
+
+// Handle create add window
+let createAddWindow = () => {
+  // Create new window
+  addWindow = new BrowserWindow({
+    width: 300,
+    height: 200,
+    title: "Add shopping list item",
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  // Load html into window
+  addWindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname, "addWindow.html"),
+      protocol: "file:",
+      slashes: true,
+    })
+  ); // file://dirname/index.html
+
+  // Garbage collection handle
+  addWindow.on("closed", function () {
+    addWindow = null;
+  });
+};
+
+// Catch item:add
+ipcMain.on("item:add", function (e, item) {
+  mainWindow.webContents.send("item:add", item);
+  addWindow.close();
+});
+
+// Create menu template
+const mainMenuTemplate = [
+  {
+    label: "File",
+    submenu: [
+      {
+        label: "Add Item",
+        accelerator: process.platform === "darwin" ? "Command+A" : "Ctrl+A",
+        click() {
+          createAddWindow();
+        },
+      },
+      {
+        label: "Clear Item",
+        accelerator: process.platform === "darwin" ? "Command+C" : "Ctrl+C",
+        click() {
+          mainWindow.webContents.send("item:clear");
+        },
+      },
+      {
+        label: "Quit",
+        accelerator: process.platform === "darwin" ? "Command+Q" : "Ctrl+Q",
+        click() {
+          app.quit();
+        },
+      },
+    ],
+  },
+];
+
+// if MAC, add empty object to menu
+if (process.platform === "darwin") {
+  mainMenuTemplate.unshift({ label: "" });
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.whenReady().then(createWindow);
-
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
-app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") {
-    app.quit();
-  }
-});
-
-app.on("activate", () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
-  }
-});
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+if (process.env.NODE_ENV !== "production") {
+  mainMenuTemplate.push({
+    label: "Developer tool",
+    submenu: [
+      {
+        label: "Toggle DevTools",
+        accelerator: process.platform === "darwin" ? "Command+I" : "Ctrl+I",
+        click(item, focusedWindow) {
+          focusedWindow.toggleDevTools();
+        },
+      },
+      {
+        role: "reload",
+      },
+    ],
+  });
+}
